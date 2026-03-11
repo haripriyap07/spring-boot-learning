7684a06026f1c63880e4ed6793cdcc4d229c1654
Haripriya <haripriya.ra23@sahyadri.edu.in>
2026-03-11 18:28:44 +0530

    Day 03 - PathVariable, RequestParam, RequestBody

---
+++ day01/src/main/java/com/example/day03/UserController.java
@@ -0,0 +1,24 @@
+
+package com.example.day03;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/user/{id}")
+    public String getUser(@PathVariable int id) {
+        return "User ID is: " + id;
+    }
+
+    @GetMapping("/search")
+    public String searchUser(@RequestParam String name) {
+        return "Searching for: " + name;
+    }
+    @PostMapping("/create-user")
+    public String createUser(@RequestBody User user){
+        return "User Created: " +user.getName() + "Age:" + user.getAge();
+    }
+
+
+}
